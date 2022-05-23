4a97f7f4b70a079ce920c9b607ab17b6a07b505e
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-23 20:34:57 +0200

    Create Patients form added

--- src/pages/CreatePatients.jsx
+++ src/pages/CreatePatients.jsx
@@ -1,11 +1,90 @@
+import { useState } from "react"
+import {useNavigate} from "react-router-dom"
+import axios from "axios"
+
+export default function CreatePatient (props) {
+
+const [firstName, setFirstName ] = useState();
+const [lastName, setLastName ] = useState();
+const [email, setEmail ] = useState();
+const [birthDate, setBirthDate ] = useState();
+const [bloodType, setBloodType ] = useState();
+const [description, setDescription ] = useState();
+
+const navigate = useNavigate();
+
+const handleSubmit = (e) => {
+    e.preventDefault();
 
 
-function CreatePatient() {
+    const newPatient = {firstName, lastName, email, birthDate, bloodType, description};
 
-    return(
-        <h1>Create a patient
-        </h1>
+     const storedToken = localStorage.getItem('authToken');
+
+
+    axios.post(process.env.REACT_APP_API_URL + "/patients",
+     newPatient,
+     { headers: { Authorization: `Bearer ${storedToken}`} }
+     )
+        .then( response => {
+            props.callbackCreatePat();
+
+            navigate("/patients")
+        })
+        .catch( e => console.log("error creating a patient, react route", e));
+     }
+
+
+
+
+return(
+
+    <section className="CreatePatient">
+    
+    <h1>Create a new patient</h1>
+        <form onSubmit={handleSubmit}>
+            <label>
+                First Name:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="text" name="firstName" value={firstName} required={true} onChange={(e) => setFirstName(e.target.value)} />
+
+            </label>
+            <label>
+               Last Name:  &nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="text" name="lastName" value={lastName} required={true} onChange={(e) => setLastName(e.target.value)} />
+
+            </label>
+            <br /><br />
+
+            <label>
+                Email: &nbsp;
+                <input type='text' name='email' value={email} required={true} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <br /><br />
+
+            <label>
+            Birth Date: &nbsp;
+                <input type='date' name='birthDate' value={birthDate} required={true} onChange={(e) => setBirthDate(e.target.value)} />
+            </label>
+
+            <label>
+            Description: &nbsp;
+                <input type='text' name='description' value={description} required={true} onChange={(e) => setDescription(e.target.value)} />
+            </label>
+            <label>
+            Blood Type: &nbsp;
+                <input type='text' name='bloodType' value={bloodType} required={true} onChange={(e) => setBloodType(e.target.value)} />
+            </label>
+            <br /><br />
+
+
+            <button type="submit">Create Patient</button>
+        </form>
+
+    </section>
     )
 }
 
-export default CreatePatient;
+
+
+
+
