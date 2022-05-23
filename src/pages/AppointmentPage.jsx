31be26745b716194d564502f5abc84cfdf6066cd
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-23 12:14:15 +0200

    Show the list of appointments

--- src/pages/AppointmentPage.jsx
+++ src/pages/AppointmentPage.jsx
@@ -1,85 +1,34 @@
-import axios from "axios";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
-function Appointments() {
+function Appointments(props){
 
-    const [date, setDate] = useState("");
-    const [doctor, setDoctor] = useState("");
-    const [patient , setPatient] = useState("");
-
-    const navigate = useNavigate();
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const newAppointment = {
-            date,
-            doctor,
-            patient,
-        }
-
-        axios.post(process.env.REACT_APP_API_URL + "/appointments", newAppointment)
-            .then(response => {
-                console.log(response.data)
-
-                navigate("/appointments"); // redirect to Appointment list
-                // navigate(`/appointments/${response.data._id}`); // redirect to Appointment page
-
-                // clear form...
-                setDate("");
-                setDoctor("");
-            })
-            .catch(e => console.log("error creating Appointment...", e));
-
+    const renderAppointments = () => {
+        const result = props.Appointments.map( (element) => {
+            return (
+                <div key={element._id} className="appointment-summary box">
+                    <p>{element.title}</p>
+                    <NavLink to="/">More details</NavLink> |&nbsp;
+                    <NavLink to={`/appointments/${element._id}/edit`}>Edit</NavLink>
+                </div>
+            )
+        });
+        return result;
     }
-
+    
     return (
-        <section className="AddAppointmentPage">
-            <h1>Create a new Appointment</h1>
+        <div className="AppointmentPage">
+            <h1>List of Appointments</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Date
-                    <input
-                        type="date"
-                        name="doctor"
-                        value={date}
-                        required={true}
-                        onChange={(e) => setDate(e.target.value)}
-                    />
-                </label>
-            
-                <label>
-                    Doctor
-                    <input
-                        type="text"
-                        name="description"
-                        value={doctor} 
-                        required={true}
-                        onChange={(e) => setDoctor(e.target.value)}
-                    />
-                </label>
+             <section>
+                 { props.Appointments === null
+                    ? <p>loading...</p>
+                    : renderAppointments()
+                }
+             </section>
 
-                <label>
-                    Patient Name
-                    <input
-                        type="text"
-                        name="patientname"
-                        value={patient} 
-                        required={true}
-                        onChange={(e) => setPatient(e.target.value)}
-                    />
-                </label>
-
-
-                <button type="submit">Create Appointment</button>
-
-            </form>
-
-        </section>
-    )
+        </div>
+    );
 }
 
 export default Appointments;
