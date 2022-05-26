57adb52fee83df86f3ff654762cd624e0d6ff6f3
alejandro <aleblancogovantes@hotmail.com>
2022-05-26 16:50:04 +0200

    added buttons to logged in

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -9,14 +9,15 @@ function Navigationbar(){
 
     return (
         <Navbar bg="dark" variant="dark" className="Navbar">
-&nbsp;&nbsp;&nbsp;<Button href="/patients/" className="links"> Patient list</Button> | &nbsp;
+
+            
+            { isLoggedIn &&
+                <>
+                &nbsp;&nbsp;&nbsp;<Button href="/patients/" className="links"> Patient list</Button> | &nbsp;
             <Button href="/patients/create" className="links">New Patient</Button> |  &nbsp;
             <Button href="/appointments/" className="links" >Appointments</Button> |||  &nbsp;
             <Button href="/appointments/create" className="links">New Appointment</Button> ||| &nbsp;
             <Button href="/profile" className="links">Profile</Button> ||| &nbsp;
-            
-            { isLoggedIn &&
-                <>
              &nbsp;&nbsp;<span className="welcome-email">Welcome, Dr. {user.email} </span> &nbsp;&nbsp;&nbsp;
                     <Button onClick={logOutUser}>Logout</Button>
                 </>
