24900ece6fc2d802a6ee47a0a088bab749b7d769
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-26 15:40:46 +0200

    Fix : reducing the Navbar to Login & Register for not logged in users

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -1,36 +1,34 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
-import { Button, Navbar } from 'react-bootstrap';
+import { Button, Navbar } from "react-bootstrap";
 
-function Navigationbar(){
+function Navigationbar() {
+  const { isLoggedIn, isLoading, user, logOutUser } = useContext(AuthContext);
 
-    const {isLoggedIn, isLoading, user, logOutUser} = useContext(AuthContext);
+  return (
+    <Navbar bg="dark" variant="dark" className="Navbar">
+      {isLoggedIn && (
+        <>
+          <NavLink to="/patients/"> Patient list</NavLink> | &nbsp;
+          <NavLink to="/patients/create">New Patient</NavLink> | &nbsp;
+          <NavLink to="/appointments/">Appointments</NavLink> ||| &nbsp;
+          <NavLink to="/appointments/create">New Appointment</NavLink> |||
+          &nbsp;
+          <NavLink to="/profile">Profile</NavLink> ||| &nbsp;
+          <span>Welcome, {user.email} </span>
+          <button onClick={logOutUser}>Logout</button>
+        </>
+      )}
 
-    return (
-        <Navbar bg="dark" variant="dark" className="Navbar">
-            <NavLink to="/patients/"> Patient list</NavLink> | &nbsp;
-            <NavLink to="/patients/create">New Patient</NavLink> |  &nbsp;
-            <NavLink to="/appointments/">Appointments</NavLink> |||  &nbsp;
-            <NavLink to="/appointments/create">New Appointment</NavLink> ||| &nbsp;
-            <NavLink to="/profile">Profile</NavLink> ||| &nbsp;
-            
-            { isLoggedIn &&
-                <>
-                    <span>Welcome, {user.email} </span> 
-                    <button onClick={logOutUser}>Logout</button>
-                </>
-            }
-
-            { !isLoggedIn &&
-                <>
-                    <NavLink to="/signup">Register</NavLink> | 
-                    <NavLink to="/login">Login</NavLink>
-                </>
-            }
-        </Navbar>
-    );
+      {!isLoggedIn && (
+        <>
+          <NavLink to="/signup">Register</NavLink> |
+          <NavLink to="/login">Login</NavLink>
+        </>
+      )}
+    </Navbar>
+  );
 }
-
 
 export default Navigationbar;
