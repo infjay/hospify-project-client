447566f8b71604ce2a13db3b2745ac967b0e6b24
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-27 09:42:25 +0200

    BootStrap implemented

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,4 +1,7 @@
 import { NavLink } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Patients(props) {
 
@@ -10,15 +13,28 @@ function Patients(props) {
   const renderPatients = () => {
     const result = props.patients.map((element) => {
       return (
-        <div key={element._id} className="patients-summary">
-          <p>First Name: {element.firstName}</p>
-          <p>Last Name: {element.lastName}</p>
-          <p>Email: {element.email}</p>
-          <p>Birthdate: {element.birthDate}</p>
-          <p>BloodType: {element.bloodType}</p>
-          <p>Description: {element.description}</p>
-          <NavLink to={`/patients/${element._id}/edit`}>Edit</NavLink>
-        </div>
+
+  <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Patinet</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted"><p>First Name: {element.firstName}</p><p>Last Name: {element.lastName}</p></Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted"><p>Email: {element.email}</p></Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted"><p>Birthdate: {element.birthDate}</p></Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted"><p>BloodType: {element.bloodType}</p></Card.Subtitle>
+    <Card.Subtitle className="mb-2 text-muted"><p>Description: {element.description}</p></Card.Subtitle>
+    <NavLink to={`/patients/${element._id}/edit`}>Edit</NavLink>
+    </Card.Body>
+   </Card>
+        
+        // <div key={element._id} className="patients-summary">
+        //   <p>First Name: {element.firstName}</p>
+        //   <p>Last Name: {element.lastName}</p>
+        //   <p>Email: {element.email}</p>
+        //   <p>Birthdate: {element.birthDate}</p>
+        //   <p>BloodType: {element.bloodType}</p>
+        //   <p>Description: {element.description}</p>
+        //   <NavLink to={`/patients/${element._id}/edit`}>Edit</NavLink>
+        // </div>
       );
     });
     return result;
